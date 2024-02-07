c735ab268a5e8e16107b2925d33ea1987d76aa8f
Aprendiz <Aprendiz@BOGDFPCGMF4147.FormacionSena.RED>
2024-02-07 16:49:15 -0500

    entidades carro y cliente con relaciones

--- app/entities.py
+++ app/entities.py
@@ -1,10 +1,24 @@
 class Carro():
-    placa = "SDF 456"
-    tipo_veiculo = "Camion"
-    
+        
+    #metodo costructor
+    def __init__(self, placa , tipo_vehiculo):
+        self.placa = placa
+        self.tipo_vehiculo = tipo_vehiculo
+
+        
 class Cliente():
-    nombre = "juan"
-    documento ="1234"
-    licencia_conduccion = "si"
-    celular = "12345"
     
+    #metodo costructor
+    def __init__(self,nombre , documento , licencia_conduccion , celular , lista_carros):
+        self.nombre = nombre
+        self.documento = documento
+        self.licencia_conduccion = licencia_conduccion
+        self.celular = celular
+        self.lista_carros = lista_carros
+        
+    def addCar(self, car):
+        self.lista_carros.append(car)
+        
+    def lisCar(self):
+        for i in self.lista_carros:
+            print("carro con placas:" + i.placa)
