89c0c9b04b61852e12cc4293d98d500c965ac4d0
Your Name <you@example.com>
2024-02-13 16:49:24 -0500

    modelo terminado

--- app/entities.py
+++ app/entities.py
@@ -22,3 +22,20 @@ class Cliente():
     def lisCar(self):
         for i in self.lista_carros:
             print("carro con placas:" + i.placa)
+            
+class Cupo():
+    
+    def __init__(self, letra):
+        self.letra = letra
+        
+class Pago():
+    
+    def __init__(self, fecha_inicio, hora_inicio, fecha_fin, hora_fin, valor,carro,cupo):
+        self.fecha_inicio = fecha_inicio
+        self.hora_inicio = hora_inicio
+        self.fecha_fin = fecha_fin
+        self.hora_fin = hora_fin
+        self.valor = valor
+        self.carro = carro
+        self.cupo = cupo
+        
